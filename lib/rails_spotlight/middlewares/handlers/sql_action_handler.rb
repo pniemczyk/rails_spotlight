84726f5d536c04969f1f3b5fabc03f7985b2cba3
Pawel Niemczyk <pawel@way2do.it>
2023-05-17 22:38:29 +0200

    Ensure all sql queries are run in save mode. Allow to use force mode

--- lib/rails_spotlight/middlewares/handlers/sql_action_handler.rb
+++ lib/rails_spotlight/middlewares/handlers/sql_action_handler.rb
@@ -8,22 +8,35 @@ module RailsSpotlight
           if ActiveSupport.const_defined?('ExecutionContext')
             ActiveSupport::Notifications.subscribed(method(:logger), 'sql.active_record', monotonic: true) do
               ActiveSupport::ExecutionContext.set(rails_spotlight: request_id) do
-                self.result = ActiveRecord::Base.connection.exec_query(query)
+                transaction
               end
             end
           else
             ActiveSupport::Notifications.subscribed(method(:logger), 'sql.active_record') do
-              self.result = ActiveRecord::Base.connection.exec_query(query)
+              transaction
             end
           end
         end
 
         private
 
+        def transaction
+          ActiveRecord::Base.transaction do
+            begin
+              self.result = ActiveRecord::Base.connection.exec_query(query)
+            rescue => e
+              self.error = e
+            ensure
+              raise ActiveRecord::Rollback unless force_execution?
+            end
+          end
+        end
+
         attr_accessor :result
+        attr_accessor :error
 
         def json_response_body
-          { result: result, logs: logs }
+          { result: result, logs: logs, error: error.inspect, query_mode: force_execution? ? 'force' : 'default' }
         end
 
         def logger(_, started, finished, unique_id, payload)
@@ -38,6 +51,10 @@ module RailsSpotlight
 
         def query
           @query ||= json_request_body.fetch('query')
+        end
+
+        def force_execution?
+          @force_execution ||= json_request_body['mode'] == 'force'
         end
       end
     end
