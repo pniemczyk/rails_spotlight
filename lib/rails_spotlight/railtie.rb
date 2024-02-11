f62e817627329c19168f1a681ca45e06d24d35d7
Pawel Niemczyk <pawel@way2do.it>
2024-02-11 20:02:04 +0100

    Improve syntax of Railtie class

--- lib/rails_spotlight/railtie.rb
+++ lib/rails_spotlight/railtie.rb
@@ -5,7 +5,7 @@ require 'rails/railtie'
 module RailsSpotlight
   class Railtie < ::Rails::Railtie
     initializer 'rails_spotlight.inject_middlewares' do
-      insert_middleware unless Rails.env.production?
+      insert_base_middlewares unless Rails.env.production?
     end
 
     initializer 'rails_spotlight.log_interceptor' do
@@ -17,19 +17,28 @@ module RailsSpotlight
     end
 
     initializer 'rails_spotlight.action_cable_setup' do
-      unless Rails.env.production?
-        app.config.after_initialize do
-          existing_origins = Array(app.config.action_cable.allowed_request_origins)
-          app.config.action_cable.allowed_request_origins = existing_origins | [%r{\Achrome-extension://.*\z}]
+      insert_action_cable_helpers unless Rails.env.production?
+    end
 
-          require 'rails_spotlight/channels/request_completed_channel' if ::RailsSpotlight.config.request_completed_broadcast_enabled?
-          require 'rails_spotlight/channels/live_console_channel' if ::RailsSpotlight.config.live_console_enabled?
-          Rails.application.routes.draw { mount ActionCable.server => '/cable' } if ::RailsSpotlight.config.auto_mount_action_cable?
-        end
+    def insert_action_cable_helpers
+      return unless ::RailsSpotlight.config.action_cable_present?
+
+      app.config.after_initialize do
+        update_actioncable_allowed_request_origins!
+
+        require 'rails_spotlight/channels/request_completed_channel' if ::RailsSpotlight.config.request_completed_broadcast_enabled?
+        require 'rails_spotlight/channels/live_console_channel' if ::RailsSpotlight.config.live_console_enabled?
+
+        app.routes.draw { mount ActionCable.server => '/cable' } if ::RailsSpotlight.config.auto_mount_action_cable?
       end
     end
 
-    def insert_middleware
+    def update_actioncable_allowed_request_origins!
+      existing_origins = Array(app.config.action_cable.allowed_request_origins)
+      app.config.action_cable.allowed_request_origins = existing_origins | [%r{\Achrome-extension://.*\z}]
+    end
+
+    def insert_base_middlewares
       app.middleware.use ::RailsSpotlight::Middlewares::RequestHandler
 
       if defined? ActionDispatch::DebugExceptions
